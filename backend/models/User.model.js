3093612ef64be15bd9d87d3ecc8de2f5247122f4
chetan <chetandubliya197@gmail.com>
2026-08-15 15:52:27 +0530

    Fix TypeError: next is not a function in Mongoose async pre-save hook

--- backend/models/User.model.js
+++ backend/models/User.model.js
@@ -89,9 +89,9 @@ const userSchema = new mongoose.Schema({
 });
 
 // Pre-save hook to hash password
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function () {
     if (!this.isModified('password')) {
-        return next();
+        return;
     }
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
