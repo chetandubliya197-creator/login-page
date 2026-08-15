1ebadaffd9b1fc0fd665103eb35a21da8bc202cb
chetan <chetandubliya197@gmail.com>
2026-08-15 15:45:57 +0530

    Fix password corruption in User model pre-save hook

--- backend/models/User.model.js
+++ backend/models/User.model.js
@@ -91,7 +91,7 @@ const userSchema = new mongoose.Schema({
 // Pre-save hook to hash password
 userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) {
-        next();
+        return next();
     }
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
